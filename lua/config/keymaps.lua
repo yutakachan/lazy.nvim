8ce904c09e4f169a6acc79af8657ca6361694669
Yutaka <yutaka.tateno@gmail.com>
2025-02-03 10:32:09 +0900

    2025-02-03

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Delete a word backwords
+-- keymap.set("n", "dw", "vd_d")
+
+-- select ALL
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+--Jump list
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New Tab
+keymap.set("n", "te", ":tabedit<Return>", opts)
+keymap.set("n", "<Tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-Tab>", ":tabprev<Return>", opts)
+
+-- Split Window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move Window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+keymap.set("n", "so", "<C-w>o")
