2b8a7efc2740d7d67ebf1792f327037036354450
WIN <yutaka.tateno@gmail.com>
2025-03-20 19:39:32 +0900

    2025-03-20 keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,9 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
 -- Delete a word backwords
 -- keymap.set("n", "dw", "vd_d")
 
@@ -15,8 +18,8 @@ keymap.set("n", "<C-m>", "<C-i>", opts)
 
 -- New Tab
 keymap.set("n", "te", ":tabedit<Return>", opts)
-keymap.set("n", "<Tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-Tab>", ":tabprev<Return>", opts)
+keymap.set("n", "tt", ":tabnext<Return>", opts)
+keymap.set("n", "tr", ":tabprev<Return>", opts)
 
 -- Split Window
 keymap.set("n", "ss", ":split<Return>", opts)
@@ -28,3 +31,9 @@ keymap.set("n", "sk", "<C-w>k")
 keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
 keymap.set("n", "so", "<C-w>o")
+
+-- resize Window
+keymap.set("n", "<C-w><C-left>", "<C-w><")
+keymap.set("n", "<C-w><C-right>", "<C-w>>")
+keymap.set("n", "<C-w><C-up>", "<C-w>+")
+keymap.set("n", "<C-w><C-down>", "<C-w>-")
